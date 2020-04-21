be0049355c5351f88934731a1906025ec5f994f4
Thien <menkisede2@gmail.com>
2020-04-21 21:07:47 +0700

    push heroku

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,9 @@ module RubyNaitei2019BookSharing
     config.time_zone = 'Asia/Ho_Chi_Minh'
     config.active_record.default_timezone = :local
 
+    # config/application.rb
+    config.assets.initialize_on_precompile = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
